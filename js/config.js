a8db699995d17377d3440993de3d966c337e3edd
Hewins37 <m.moise1937@gmail.com>
2025-07-16 19:42:06 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Xj1eNm4Tk', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7ahg45kbg0022p0hs19ncdb3me', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_bo52TONhd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '25fj2gsammulisilce396e3f7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
